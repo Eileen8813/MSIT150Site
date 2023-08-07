0889584924aabab530804624d99961bf20ea3365
Eileen <shingirl520@gmail.com>
2023-08-07 19:32:48 +0800

    作業2的JSON抓不到

--- Controllers/HomeWorkController.cs
+++ Controllers/HomeWorkController.cs
@@ -4,10 +4,15 @@ namespace MSIT150Site.Controllers
 {
     public class HomeWorkController : Controller
     {
-        public IActionResult himeworl1()
+        public IActionResult homework1()
         {//作業一 改用 jQuery 完成JSON資料讀取並顯示在表格中的練習
             return View();
             
+        }
+        public IActionResult homework2()
+        {//作業二 引用 datas/Travel.js 中的JSON，讀取資料後，透過 bootstrap cards component 顯示在網頁上
+            return View();
+
         }
     }
 }
